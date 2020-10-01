d2611f2deda6bd5ffd09a82009a9c34ada48592a
Farrel Ahmad Yudithia <farrel.yudithia@gmail.com>
2020-10-01 15:17:04 +0700

    Style

---
+++ STYLE_CERC/app/src/main/java/com/example/style/Opening.java
@@ -0,0 +1,32 @@
+package com.example.style;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Opening extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_opening);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+}}
